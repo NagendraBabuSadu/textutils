55c64da0fa529c798aed7f4ba0fb64d5da2856d4
NagendraBabuSadu <nagendrababusadu@outlook.com>
2022-08-08 05:53:23 +0530

    TextUtils

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+// eslint-disable-next-line
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils"  aboutPage="AboutText"/>
+      {/* <Navbar/> */}
+
+
+     
+     </>
+  );
+}
+
+export default App;
