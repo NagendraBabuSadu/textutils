486cec1b16696703c509e9da989afb043d015ee3
NagendraBabuSadu <nagendrababusadu@outlook.com>
2022-08-10 08:26:11 +0530

    About Page with Dark mode enabled

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 // eslint-disable-next-line
 function App() {
   return (
     <>
-      <Navbar title="TextUtils" aboutPage="AboutText" />
+      {/* <Navbar title="TextUtils" aboutPage="AboutText" /> */}
       {/* <Navbar/> */}
-      <div className="container">
-        <TextForm heading="Enter your Text to Analyze" />
+      <Navbar title="TextUtils" aboutPage="About" />
+
+      <div className="container" my-3>
+        {/* <TextForm heading="Enter your Text to Analyze" /> */}
+        <About/>
       </div>
+      
     </>
   );
 }
